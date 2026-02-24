d07841ec50e0d0256570aa4d0a4938c810fd3231
RON-000000 <schwaro4@students.zhaw.ch>
2026-02-24 09:06:08 +0100

    Added country and state

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -1,7 +1,9 @@
 package ch.zhaw.avalanger.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/avalange")    
 public class AvalangeController {
     
-    @GetMapping("")
-    public String getAllAvalanges() {
-        return "No Avalanges found";
+    @GetMapping({"", "/{country}"})
+    public String getAllAvalanges(@PathVariable(required = false) String country, 
+                                  @RequestParam(required = false) String state) {
+        if (country == null || country.isEmpty()) {
+            return "No Avalanges found";
+        }
+        return "No Avalanges found for country: " + country + " and state: " + state;
     }
     
 
